f601a7dad358d138cdf6366c0f0a30b530e325f7
Douwe <douwe.schepers@rentman.nl>
2023-10-23 13:55:08 +0200

    [RM4-33329] Change full_name column to generated column

--- Films and Actors/1 - AddFullName.sql
+++ Films and Actors/1 - AddFullName.sql
@@ -3,7 +3,25 @@ All about names: The actor table has their value capitalized, which is uncommon 
 We probably want to use this column a lot, make a generated column out of it
  */
 ALTER TABLE actor
-ADD full_name varchar(255);
-UPDATE actor
-SET
-    full_name = CONCAT(LOWER(first_name),' ', LOWER(last_name));
+ADD COLUMN full_name varchar(255) GENERATED ALWAYS as (
+    CONCAT(
+        CONCAT(
+            UCASE(
+                LEFT(
+                    LOWER(actor.first_name),1
+                )
+            ),
+            SUBSTRING(LOWER(actor.first_name),2)
+        ),
+	' ',
+        CONCAT(
+            UCASE(
+                LEFT(
+                    LOWER(
+                        actor.last_name),1
+                )
+            ),
+            SUBSTRING(LOWER(actor.last_name),2)
+        )
+    )
+) STORED
